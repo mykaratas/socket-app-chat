a7778d14a1df75632e7d267bd6a37eeb76c051e1
Malik Karatas <malik.karatas@atez.com.tr>
2021-05-24 10:55:12 +0300

    added port env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
 
